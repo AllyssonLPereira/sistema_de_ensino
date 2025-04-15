ed02ef7c93b924141dc398629fd1885f1c1a4919
AllyssonLPereira <allyssonlunguinho.03i@gmail.com>
2025-04-15 20:22:28 -0300

    refatorando o metodo 'displayInformation' para lidar com argumentos

--- User.js
+++ User.js
@@ -1,7 +1,7 @@
 export default class User {
-    
+
     // using only private attributes (#), we can still have problems, because assigning a value to a property that does not exist creates that property. For example, newUser.name = "Marcio", would create the prop. name.
-    
+
     #name
     #email
     #birth
@@ -45,21 +45,15 @@ export default class User {
         this.#name = newName;
     }
 
-    // displayInformation() {
-    //     return `Name: ${this.name}\nEmail: ${this.email}`;
-    // }
-
     // Overload = sobrecarga de método (muito acoplamento) *Ele não consegue usar a ASSINATURA DE FUNÇÃO
     displayInformation() {
-        if (this.role === "student") {
-            return `Student: ${this.name}`;
+        const types = {
+            student: `Student: ${this.name}`,
+            admin: `Admin: ${this.name}\nEmail: ${this.email}`,
+            teacher: `Teacher: ${this.name}\nEmail: ${this.email}`
         }
-        if (this.role === "admin") {
-            return `Admin: ${this.name}\nEmail: ${this.email}`;
-        }
-        if (this.role === "teacher") {
-            return `Teacher: ${this.name}\nEmail: ${this.email}`;
-        }
+
+        return types[arguments[0]]
     }
 
     // Static methods do not depend on instances or class constructors
@@ -67,6 +61,3 @@ export default class User {
         return `Name: ${name}\nEmail: ${email}`
     }
 }
-
-// const newUser = new User("Allysson", "allysson@domain.com", "2025/01/01");
-// console.log(newUser.displayInformation());
--- index.js
+++ index.js
@@ -3,17 +3,10 @@ import Admin from "./Admin.js";
 import Teacher from "./Teacher.js";
 
 const newUser = new User("Allysson", "allysson@domain.com", "1980/02/02");
-console.log(newUser.displayInformation());
+console.log(newUser.displayInformation("student"));
 
 const dataFictitious = User.displayInformationGeneric("Yoda", "yoda@dagobah.com");
 console.log(dataFictitious);
-
-
-// const newAdmin = new Admin("Fernando", "fernando@domain.com", "1980/02/02");
-// console.log(newAdmin.displayInformation());
-
-// const newTeacher = new Teacher("Milene", "milene@domain.com", "1989/03/27");
-// console.log(newTeacher.displayInformation());
 
 // newAdmin.name = "";
 // console.log(newAdmin.name); -> It is called as if it were a prop. It does not know about the internal implementation of the Parent class.
