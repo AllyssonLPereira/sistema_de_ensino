a84796632218a74a53acbceaa7b81f7571671422
AllyssonLPereira <allyssonlunguinho.03i@gmail.com>
2025-04-15 16:35:00 -0300

    adicionando a modelagem do codigo, o arquivo 'User.js' e o package.json

---
+++ User.js
@@ -0,0 +1,16 @@
+export default class User {
+    constructor(name, email, birth, role, active = true) {
+        this.name = name
+        this.email = email
+        this.birth = birth
+        this.role = role || "estudante"
+        this.active = active
+    }
+
+    displayInformation() {
+        console.log(`Name: ${this.name}\nEmail: ${this.email}`)
+    }
+}
+
+const newUser = new User("Allysson", "allysson@domain.com", "2000/01/01", "CISO", true)
+console.log(newUser.displayInformation());
